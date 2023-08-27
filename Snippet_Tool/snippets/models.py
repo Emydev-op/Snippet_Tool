50fc03083d8f26128b3c4c0b7c9a3818293ccfbe
Emydev-op <alphaoseghe@gmail.com>
2023-08-27 13:42:03 +0100

    made changes in the models

--- Snippet_Tool/snippets/models.py
+++ Snippet_Tool/snippets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 # Create your models here.
