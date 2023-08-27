d6d2c9f88886d4d3db9aa1e0f7467f5de5e9548f
Emydev-op <alphaoseghe@gmail.com>
2023-08-27 13:02:27 +0100

    created the class-based view for the snippets

--- Snippet_Tool/snippets/views.py
+++ Snippet_Tool/snippets/views.py
@@ -1,3 +1,13 @@
+from rest_framework import generics
 from django.shortcuts import render
-
+from .models import Snippet
+from .serializers import SnippetSerializer
 # Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
