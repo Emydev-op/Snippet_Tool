eda2f2c7715671585b4d376ccb4c4f55a008e923
Emydev-op <alphaoseghe@gmail.com>
2023-08-27 21:12:04 +0100

    resolved some issues with the highlighted field error

--- Snippet_Tool/snippets/admin.py
+++ Snippet_Tool/snippets/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Snippet
 # Register your models here.
 
-admin.site.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields= ('highlighted')
+
+admin.site.register(Snippet, SnippetAdmin)
--- Snippet_Tool/snippets/models.py
+++ Snippet_Tool/snippets/models.py
@@ -18,9 +18,27 @@ class Snippet(models.Model):
         choices=LANGUAGE_CHOICES, default="python", max_length=100
     )
     style = models.CharField(choices=STYLE_CHOICES, default="friendly", max_length=100)
-
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
+    
     class Meta:
         ordering = ['created']
         
+    def save(self, *args, **kwargs):  # new
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = "table" if self.linenos else False
+        options = {"title": self.title} if self.title else {}
+        formatter = HtmlFormatter(
+            style=self.style, linenos=linenos, full=True, **options
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
+        
     def __str__(self):
         return self.title
+    
+    
