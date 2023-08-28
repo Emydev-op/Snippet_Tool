a492d098a560094021d46e317d00b25b22aa049e
Emydev-op <alphaoseghe@gmail.com>
2023-08-28 08:26:41 +0100

    created the views for the user

--- Snippet_Tool/snippets/serializers.py
+++ Snippet_Tool/snippets/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "snippets")
--- Snippet_Tool/snippets/views.py
+++ Snippet_Tool/snippets/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 # Create your views here.
 
 class SnippetList(generics.ListCreateAPIView):
@@ -11,3 +12,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
