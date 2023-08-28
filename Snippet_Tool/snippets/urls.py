b55c593269c104d7a5be47329580153087e444cf
Emydev-op <alphaoseghe@gmail.com>
2023-08-28 08:27:46 +0100

    created the user url routers

--- Snippet_Tool/snippets/urls.py
+++ Snippet_Tool/snippets/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippetList'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippetDetail'),
+    path("users/", views.UserList.as_view()),  # new
+    path("users/<int:pk>/", views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
