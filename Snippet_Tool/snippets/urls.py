429534a526057960f1fe1d2a560de9cf3a3b70f9
Emydev-op <alphaoseghe@gmail.com>
2023-09-19 15:40:02 +0100

    created highlighted snippet endpoint

--- Snippet_Tool/snippets/urls.py
+++ Snippet_Tool/snippets/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippetList'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippetDetail'),
-    path("users/", views.UserList.as_view()),  # new
+    path("users/", views.UserList.as_view()), 
     path("users/<int:pk>/", views.UserDetail.as_view()),
+    path("snippets/<int:pk>/highlight/", views.SnippetHighlight.as_view(), name="snippet-highlight",),
     path("", views.api_root),
 ]
 
--- Snippet_Tool/snippets/views.py
+++ Snippet_Tool/snippets/views.py
@@ -1,13 +1,20 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view  
 from rest_framework.response import Response  
 from rest_framework.reverse import reverse
 
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):  
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 @api_view(["GET"])  
 def api_root(request, format=None):
